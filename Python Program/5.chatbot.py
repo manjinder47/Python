cf04e2cf4fdc4ec0a57ed142637dbb0f5a66b606
Hharshitsingh <realharsgitsingh@gmail.com>
2020-07-14 18:00:52 +0530

    update

--- Python Program/5.chatbot.py
+++ Python Program/5.chatbot.py
@@ -1,10 +1,24 @@
-chbt = {"what is your name": "I'm Bot","how old are you": "1 Day","where do you live":"I'm Everywhere","what does you do":"Nothing"}
-print("\t\tWELCOME\n")
+import sys
+import os
+os.system('clear')
+os.system('setterm -cursor off')
+chbt = {"hi": "\nHello👋\n", "who are you": "\nI'm bot🤖\n", "what is your name": "\nArsenal\nMay I know your Name??\n","how are you":"Good😉\nHow are you" ,"what can you do": "\nAnything 😜\n","how old are you": "\n1 Day✨\n", "where do you live": "\nI'm Everywhere🌏\n","harsh":"\nOk\n","good":"Ok","ok":"\nHmm🙂\n"}
+print("\t\t\t\tWELCOME\n")
+f = open("5.chatbot.txt", 'a')
 while True:
-    a=input("Enter your Question: ")
-    b=a.lower()
-    print(chbt[b])
-    i = input("If you want to ask more Questions Enter Y: ")
-    if(i!='y' and i!='Y'):
-        break
+    a = input(" \t\t\t\t\t\t\t")
+    f.write(a)
+    f.write("\n")
+    b = a.lower()
+    try:
+        print(chbt[b])
+        f.write(chbt[b])
+        f.write("\n")
+    except:
+        print("Sorry, I can't your answer your Question")
+        i = input("If you want to ask more Questions Enter Y: ")
+        if(i != 'y' and i != 'Y'):
+             break
 print("Thank You!")
+f.close()
+os.system('setterm -cursor on')
